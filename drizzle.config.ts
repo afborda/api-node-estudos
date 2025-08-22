5d248f43db00783ecdc27ff6e1285b3957205f3a
cast <cast@casts-MacBook-Pro.local>
2025-08-22 16:07:42 -0300

    feat(database): update database schema and migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,14 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in the environment variables');
 }
 
+const databaseUrl = process.env.NODE_ENV === 'production' 
+  ? `${process.env.DATABASE_URL}?sslmode=require`
+  : process.env.DATABASE_URL;
+
 export default defineConfig({
   dialect: 'postgresql',
   dbCredentials:{
-    url: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    url: databaseUrl,
   },
   out:'./drizzle',
   schema:'./src/database/schema.ts',
--- src/database/client.ts
+++ src/database/client.ts
@@ -1,13 +1,11 @@
 import {drizzle} from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in environment variables')
 }
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-})
+const databaseUrl = process.env.NODE_ENV === 'production' 
+  ? `${process.env.DATABASE_URL}?sslmode=require`
+  : process.env.DATABASE_URL;
 
-export const db = drizzle(pool)
+export const db = drizzle(databaseUrl)
