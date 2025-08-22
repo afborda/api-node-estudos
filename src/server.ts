32b2a26d8e438a06d58559a9809010794e2f8a6f
cast <cast@casts-MacBook-Pro.local>
2025-08-22 10:53:48 -0300

    test: add/update test suite

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import { server } from "./app.ts"
 
+async function start() {
+    try {
+        await server.ready();
+        await server.listen({ port: 3000, host: '0.0.0.0' });
+        console.log('Server listening on http://0.0.0.0:3000');
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+}
 
-
-server.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
-    console.log('Server listening on http://localhost:3000')
-})
+start();
 
